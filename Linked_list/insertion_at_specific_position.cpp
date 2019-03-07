b812d079995a5e7d9c1046cc08a405d4fc9e6219
Ravi Kumar Singh <imrk016@gmail.com>
2019-03-07 18:29:21 +0530

    Update insertion_at_specific_position.cpp

--- Linked_list/insertion_at_specific_position.cpp
+++ Linked_list/insertion_at_specific_position.cpp
@@ -9,14 +9,37 @@ struct node {
     link=NULL;
   }
 };
-void insert_data(node* &head, int item) {
-  if(head==NULL)
-    head=new node(item);
+
+bool check_pos(int pos,node* head){
+  int size=0;
+  while(head!=NULL){
+    size++;
+    head=head->link;
+  }
+  if(size+1>=pos && pos>=1)
+    return true;
+  return false;
+}
+
+void insert_data(node* &head, int item,int position) {
+  if(check_pos(position,head)){
+    node* new_node=new node(item);
+    if(position==1){
+      new_node->link=head;
+      head=new_node;
+    }
+    else{
+      node* temp=head;
+      while(position!=2) {
+        temp=temp->link;
+        position--;
+      }
+      new_node->link=temp->link;
+      temp->link=new_node;
+    }
+  }
   else{
-    node* temp=head;
-    while(temp->link!=NULL)
-      temp=temp->link;
-    temp->link=new node(item);
+    cout<<"Invalid Position !";
   }
 }
 
@@ -29,12 +52,12 @@ void show_data(node* head) {
 
 int main() {
   node* HEAD=NULL;
-  insert_data(HEAD,44);
-  insert_data(HEAD,15);
-  insert_data(HEAD,23);
-  insert_data(HEAD,19);
-  insert_data(HEAD,43);
-  insert_data(HEAD,18);
+  insert_data(HEAD,44,1);
+  insert_data(HEAD,15,2);
+  insert_data(HEAD,23,1);
+  insert_data(HEAD,19,4);
+  insert_data(HEAD,43,3);
+  insert_data(HEAD,18,1);
   cout<<"Linked list : ";
   show_data(HEAD);
   return 0;
