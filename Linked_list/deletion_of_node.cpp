40abbd1a5fb1f3b260ef4fc814321369b9385ee7
Ravi Kumar Singh <imrk016@gmail.com>
2019-03-07 19:12:38 +0530

    updating

--- Linked_list/deletion_of_node.cpp
+++ Linked_list/deletion_of_node.cpp
@@ -26,6 +26,34 @@ void show_data(node* head) {
     head=head->link;
   }
 }
+bool check_pos(node* head,int pos){
+  int size=0;
+  while(head!=NULL){
+    size++;
+    head=head->link;
+  }
+  if(size>=pos && pos>=1)
+    return true;
+  return false;
+}
+
+void delete_node(node* &head,int position){
+  if(position==1){
+    node* temp=head;
+    head=head->link;
+    delete temp;
+  }
+  else{
+    node* temp=head;
+    while(position!=2){
+      temp=temp->link;
+      position--;
+    }
+    node* d_node=temp->link;
+    temp->link=d_node->link;
+    delete d_node;
+  }
+}
 
 int main() {
   node* HEAD=NULL;
@@ -36,6 +64,18 @@ int main() {
   insert_data(HEAD,43);
   insert_data(HEAD,18);
   cout<<"Linked list : ";
+  show_data(HEAD);
+  int position;
+  label:
+  cout<<"\nEnter Positon for Deletion : ";
+  cin>>position;
+  if(check_pos(HEAD,position))
+    delete_node(HEAD,position);
+  else{
+    cout<<"Invalid Position!";
+    goto label;
+  }
+  cout<<"\nAfter deletion , Linked List : ";
   show_data(HEAD);
   return 0;
 }
