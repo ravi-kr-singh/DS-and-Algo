8c706dc228263d66c2afd29b42edb2c4c8a40544
Ravi Kumar Singh <imrk016@gmail.com>
2019-03-12 20:33:46 +0530

    updating prog

--- Linked_list/19.rotate_list.cpp
+++ Linked_list/19.rotate_list.cpp
@@ -30,10 +30,37 @@ void show_data(node* head) {
   }
 }
 
+int list_size(node* h){
+  int n=0;
+  while(h!=NULL){
+    n++;
+    h=h->link;
+  }
+  return n;
+}
 
 
 node* rotate_list(node* head,int k){
-    while()
+    node* temp=head;
+    int size=list_size(head);
+    k=k%size;
+    if(k==0){
+      return  head;
+    }
+    int m=size-k;
+    if(m==0)
+      return head;
+    while(m-1!=0){
+      head=head->link;
+      m--;
+    }
+    node* new_head=head->link;
+    node* t=new_head;
+    while(t->link!=NULL)
+      t=t->link;
+    t->link=temp;
+    head->link=NULL;
+    return new_head;
 }
 
 int main() {
@@ -45,6 +72,12 @@ int main() {
   insert_data(HEAD,43);
   insert_data(HEAD,18);
   cout<<"Linked list : ";
+  show_data(HEAD);
+  int k;
+  cout<<"\nk :";
+  cin>>k;
+  cout<<"\nAfter "<<k<<" rotations, Linked list : ";
+  HEAD=rotate_list(HEAD,k);
   show_data(HEAD);
   return 0;
 }
